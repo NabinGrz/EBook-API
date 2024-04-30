f8174f2d46b5f44104fb4db94b635c0f7e606896
Nabin.Grz <nobig22@gmail.com>
2024-04-30 21:33:31 +0545

    feat: setup global erro handler

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
-import express from 'express'
-
+import express, { Request, Response, NextFunction } from "express";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 const app = express();
 
 //ROUTES
 //HTTP METHODS
 
-app.get('/',(req, res, next) => {
-    res.json({
-        message: "Welcome the Ebook API'S"
-    })
-})
+app.get("/", (req, res, next) => {
+  res.json({
+    message: "Welcome the Ebook API'S",
+  });
+});
+
+//GLOBAL ERROR HANDLER
+//***MAKE SURE IS AT LAST OF ROUTES
+
+app.use(globalErrorHandler);
 
 export default app;
