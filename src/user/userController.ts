bbbf0e7a002fda638f1bbd03d93f184520122a09
Nabin.Grz <nobig22@gmail.com>
2024-04-30 22:10:00 +0545

    feat: added user model

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,7 @@
 import createHttpError from "http-errors";
 import userRouter from "./userRouter";
 import { Request, Response, NextFunction } from "express";
+import userModel from "./userModel";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -13,7 +14,16 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     //?BELOW CAN BE DONE AS WELL
     // return res.json({ message: "All fields are required" });
   }
-
+  //!DATABASE
+  //   const user = await userModel.findOne({ email: email }); //?IF NAME OF KEY & VALUE ARE SAME
+  const user = await userModel.findOne({ email });
+  if (user) {
+    const error = createHttpError(
+      400,
+      "User already registered with this email"
+    );
+    return next(error);
+  }
   //!PROCESS
   //!RESPONSE
   res.json({ message: "User Registered" });
