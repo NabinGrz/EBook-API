ff2a253af740fba7db8f5e0e4cdd37e483ddcb07
Nabin.Grz <nobig22@gmail.com>
2024-05-01 09:17:15 +0545

    feat: create book

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,28 +1,32 @@
 import { Request, Response, NextFunction } from "express";
 import cloudinary from "../config/cloudinary";
 import path from "node:path";
-
+import bookModel from "./bookModel";
+import fs from "node:fs";
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
-  //   const {} = req.body;
+  const { title, genre } = req.body;
   console.log("files", req.files);
-  let uploadResult;
+  let coverImageUploadResult;
   let bookFileUploadResult;
   try {
     //? THIS IS TYPE CASTING TO MULTER
     const files = req.files as { [fieldname: string]: Express.Multer.File[] };
     const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
     const filename = files.coverImage[0].filename;
-    const filePath = path.resolve(
+    const coverImagefilePath = path.resolve(
       __dirname,
       "../../public/data/upload",
       filename
     );
 
-    uploadResult = await cloudinary.uploader.upload(filePath, {
-      filename_override: filename,
-      folder: "book-covers",
-      format: coverImageMimeType,
-    });
+    coverImageUploadResult = await cloudinary.uploader.upload(
+      coverImagefilePath,
+      {
+        filename_override: filename,
+        folder: "book-covers",
+        format: coverImageMimeType,
+      }
+    );
     //**------------------------------------------------------ */
     const bookPDFMimeType = files.file[0].mimetype.split("/").at(-1);
     const bookFileName = files.file[0].filename;
@@ -38,11 +42,29 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
       folder: "book-pdfs",
       format: bookPDFMimeType,
     });
+
+    const newBook = await bookModel.create({
+      title,
+      genre,
+      author: "6631b63920fea3e59e14ceb1",
+      coverImage: coverImageUploadResult.secure_url,
+      file: bookFileUploadResult.secure_url,
+    });
+    try {
+      //**DELETE TEMP FILES */
+      await fs.promises.unlink(coverImagefilePath);
+      await fs.promises.unlink(bookFilPath);
+      res.status(201).json({
+        id: newBook._id,
+      });
+    } catch (error) {
+      return next(error);
+    }
   } catch (error) {
     return next(error);
   }
 
-  console.log("files", uploadResult);
+  console.log("files", coverImageUploadResult);
   console.log("files", bookFileUploadResult);
   res.json({});
 };
