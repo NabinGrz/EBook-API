9816898f4944705d5f6873d8fa0a07b6edd8f004
Nabin.Grz <nobig22@gmail.com>
2024-05-01 07:37:26 +0545

    feat: add error handling

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -5,6 +5,7 @@ import userModel from "./userModel";
 import bcrypt from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
+import { User } from "./userTypes";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -18,37 +19,49 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     // return res.json({ message: "All fields are required" });
   }
   //!DATABASE
-  //   const user = await userModel.findOne({ email: email }); //?IF NAME OF KEY & VALUE ARE SAME
-  const user = await userModel.findOne({ email });
-  if (user) {
-    const error = createHttpError(
-      400,
-      "User already registered with this email"
-    );
-    return next(error);
+  try {
+    //   const user = await userModel.findOne({ email: email }); //?IF NAME OF KEY & VALUE ARE SAME
+    const user = await userModel.findOne({ email });
+    if (user) {
+      const error = createHttpError(
+        400,
+        "User already registered with this email"
+      );
+      return next(error);
+    }
+  } catch (error) {
+    return next(createHttpError(500, "Error while getting user"));
   }
   //!PASSWORD- HASH
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = await userModel.create({
-    name,
-    email,
-    password: hashedPassword,
-  });
-
-  //**JWT TOKEN GENERATE */
-  const token = sign(
-    {
-      sub: newUser._id,
-    },
-    config.jwtSecret as string,
-    {
-      expiresIn: "7d",
-      algorithm: "HS256", //? THIS IS DEFAULT
-    }
-  );
-  //!PROCESS
-  //!RESPONSE
-  res.json({ accessToken: token });
+  let newUser: User;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    newUser = await userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+  } catch (error) {
+    return next(createHttpError(500, "Error while creating user"));
+  }
+  try {
+    //**JWT TOKEN GENERATE */
+    const token = sign(
+      {
+        sub: newUser._id,
+      },
+      config.jwtSecret as string,
+      {
+        expiresIn: "7d",
+        algorithm: "HS256", //? THIS IS DEFAULT
+      }
+    );
+    //!PROCESS
+    //!RESPONSE
+    res.json({ accessToken: token });
+  } catch (error) {
+    return next(createHttpError(500, "Error while signing the JWT Token"));
+  }
 };
 
 export { createUser };
